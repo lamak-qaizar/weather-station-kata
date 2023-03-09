829b046921eafc0033752ae5025a8e6c5db9f054
lamak <lamak.qaizar@gmail.com>
2023-03-09 16:25:45 +0500

    updated print statements so temp is printed on the same line

--- src/main/java/WeatherStation.java
+++ src/main/java/WeatherStation.java
@@ -2,13 +2,13 @@ public class WeatherStation {
 
     public void checkTemperature() {
         Sensor sensor = new Sensor();
-        System.out.println("Dear villagers. The current weather is...");
+        System.out.print("Dear villagers. The current weather is ");
         Double value = sensor.getReading();
         if (value == null) {
-            System.out.println("No reading! Please check back soon.");
+            System.out.println("(no reading)! Please check back soon.");
         } else {
             value = (value - 32) * 5 / 9;
-            System.out.println(value + " C");
+            System.out.println(value + " C.");
         }
     }
 }
